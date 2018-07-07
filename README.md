678ce4d8884e877a27e4a10c24980c5a8f9ae879
Declan Whelan <declan.whelanbell.ca@Declans-MacBook-Pro.local>
2018-07-07 18:16:04 -0400

    r Updated readme and some simple renames

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
 # Rnc
 
-**TODO: Add description**
-
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `rnc` to your list of dependencies in `mix.exs`:
-
-```elixir
-def deps do
-  [
-    {:rnc, "~> 0.1.0"}
-  ]
-end
-```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/rnc](https://hexdocs.pm/rnc).
-
+A solution to the roman numeral kata in Elixir.
--- test/rnc_test.exs
+++ test/rnc_test.exs
@@ -1,7 +1,7 @@
 defmodule RncTest do
   use ExUnit.Case
 
-  %{
+  test_cases = %{
     1 => "I",
     2 => "II",
     3 => "III",
@@ -28,12 +28,13 @@ defmodule RncTest do
     1000 => "M",
     3999 => "MMMCMXCIX"
   }
-  |> Enum.each(fn {arabic, expected} ->
+  
+  test_cases |> Enum.each(fn {arabic, expected_roman} ->
     @arabic arabic
-    @expected expected
+    @expected_roman expected_roman
 
-    test "Rnc.convert(#{@arabic}) => #{@expected}" do
-      assert Rnc.convert(@arabic) == @expected
+    test "Rnc.convert(#{@arabic}) => #{@expected_roman}" do
+      assert Rnc.convert(@arabic) == @expected_roman
     end
   end)
 end
